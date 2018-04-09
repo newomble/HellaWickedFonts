b47ec44204555be12055445ebc7302e47ab2da65
Erika Tobias <et5392@rit.edu>
2018-04-08 22:26:51 -0400

    Search page for the user
    Created a search page which calls the Search object previously used in the home page.

---
+++ src/search.php
@@ -0,0 +1,22 @@
+<?php
+	require_once(__DIR__.'/resources/php/utility.php');
+	require_once(__DIR__.'/resources/php/top_layout.php');
+?>
+		
+		
+<div class="content center">
+	<div id="search_container"></div>
+	<br><br>
+</div>
+
+
+
+<?php
+	require_once(__DIR__.'/resources/php/bottom_layout.php');
+?>
+
+
+<script src="/resources/js/search.js"></script>
+<script>
+	var s = new Search(true, true);	
+</script>
