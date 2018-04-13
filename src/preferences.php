4c60cd674fcf39c48c77fc85a3d19bc9619b3d4f
Erika Tobias <et5392@rit.edu>
2018-04-12 21:43:04 -0400

    Error handling - message display
    Displays errors for individual fields.

--- src/preferences.php
+++ src/preferences.php
@@ -7,10 +7,11 @@
 			<h3><i class="fas fa-pencil-alt"></i> Update Your Preferences</h3>
 
 			<form name="preferencesForm" action="">
-				<p id="errors"></p>
+				<p id="errors" class="error"></p>
 
 				<label for="email">Email</label>
 				<input class="text" type="text" id="email" name="email" placeholder="johndoe@gmail.com"><br>
+				<label> </label><i class="info">HellaWickedFonts uses <a href='https://gravatar.com/' target='_blank'>Gravatar</a> for icons. Consider using your <a href='https://gravatar.com/' target='_blank'>gravatar</a> email address.</i><br><br>
 
 				<label for="fname">First Name</label>
 				<input class="text" type="text" id="fname" name="firstname" placeholder="First Name"><br>
@@ -35,7 +36,7 @@
 			<br/>
 
 			<div class="right">
-				<input class="edit_buttons" type="submit" onclick= "return validatePreferencesForm()" value="Save">
+				<input class="edit_buttons" type="submit" onclick= "return up.validateFields()" value="Save">
 				<input class="edit_buttons" type="submit" onclick="cancel()" value="Cancel">
 			
 			</div>
