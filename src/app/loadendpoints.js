c2f7b7f4cd4c4208dfd6dddb51c3f242ee774130
Student <student@raj36.istlabs.rit.edu>
2018-04-16 13:55:02 -0400

    resources available through node

--- src/app/loadendpoints.js
+++ src/app/loadendpoints.js
@@ -1,7 +1,18 @@
-var fontModel = require('./data/font.model.js');
+var path = require('path'),
+    resRoot = path.dirname(require.main.filename) + "/app/resources";
+
 
 module.exports = function(app){
     app.get('/', function (req, res) {
         res.send('Hello World!');
+    });
+
+    //Example: /css/main
+    app.get('/css/:fileName',function(req,res){
+        res.sendfile(req.params["fileName"]+".css", {root: resRoot+"/css"});
+    });
+    //Example: /js/collection
+    app.get('/js/:fileName', function(req,res){
+        res.sendfile(req.params["fileName"]+".js", {root: resRoot+"/js"});
     });
 };
