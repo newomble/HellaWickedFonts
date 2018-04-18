ba18f53a6c2df611491072e865db7d10eb8a6c72
newomble <newomble@outlook.com>
2018-04-17 20:33:39 -0400

    page routing

--- src/app/loadendpoints.js
+++ src/app/loadendpoints.js
@@ -1,22 +1,17 @@
 var path = require('path'),
     basePath = path.dirname(require.main.filename),
     resRoot =  basePath + "/app/resources",
-    apiRouter = require(basePath+'/app/router/api.router.js');
+    apiRouter = require(basePath+'/app/router/api.router.js'),
+    express = require('express');
 
 
 module.exports = function(app){
     app.get('/', function (req, res) {
-        res.send('Hello World!');
+        res.sendfile("index.html",{root:resRoot+"/pages"});
     });
     
-    app.get('/api',apiRouter);
-
-    //Example: /css/main
-    app.get('/css/:fileName',function(req,res){
-        res.sendfile(req.params["fileName"]+".css", {root: resRoot+"/css"});
-    });
-    //Example: /js/collection
-    app.get('/js/:fileName', function(req,res){
-        res.sendfile(req.params["fileName"]+".js", {root: resRoot+"/js"});
-    });
+    app.use('/api',apiRouter);
+    app.use('/page/', express.static(resRoot+"/pages") );
+    app.use('/css/',  express.static(resRoot+"/css") );
+    app.use('/js/',express.static(resRoot+"/js" ));
 };
