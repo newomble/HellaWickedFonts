16b1bdbaf77d71b9c33046deaea03e6e2988f0e4
= <=>
2018-04-20 13:49:33 -0400

    user face

--- src/app/controlla/user.face.js
+++ src/app/controlla/user.face.js
@@ -1,17 +1,30 @@
-
+var usrModel = require("../data/user.model.js"),
+    collModel = require("../data/user_font.model.js");
 
 
 function login(uName,pword){//return uid
-
+    user = usrModel.getCredentials(uName);
+    if(user[0].password == pword){
+        return user.id;
+    }
+    return false;
 }
-function newCollection(uid,fontIdArr){//return boo
-
+function newCollection(uid,fontIdArr){//return stuff
+    results = {};
+    fontIdArr.forEach(function(fid){
+        if( !collModel.add(uid,fid)){
+            results.failed.fontId = fid;
+        }else{
+            results.success.fontId = fid;
+        }
+    })
+    return results;
 }
 function getCollections(uid){//all users collections
-
+    return collModel.getFromUser(uid);
 }
 function getCollection(cid){//specific collection
-
+    return false;
 }
 module.exports = {
     login:login,
--- src/app/data/user_font.model.js
+++ src/app/data/user_font.model.js
@@ -3,9 +3,14 @@ const conn = require("./db.js");
 const fromuserQuery = "select * from sample_text where user_user_id = $1",
 	fromfontQuery = "select * from sample_text where font_font_id = $1",
 	updateQuery = "",
-	deleteQuery = "";
+	deleteQuery = "",
+	addQuery = "";
 function getFromUser(id){
 	return conn.execute(fromuserQuery,[id]);
+}
+
+function add(uid,fid){
+	return conn.execute(addQuery,[fid]);
 }
 
 module.exports = {
