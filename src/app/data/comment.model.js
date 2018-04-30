b30e90b62aaab0ee6aa128a95189e897b0a90085
newomble <newomble@outlook.com>
2018-04-30 03:33:47 -0400

    inserts get response\

--- src/app/data/comment.model.js
+++ src/app/data/comment.model.js
@@ -1,12 +1,14 @@
 const conn = require("./db.js");
 
-const getFromNameQuery = "select family,source_json,popularity,kind,username,comment_text,comment_id,public.user.user_id, AVG(rating.rating) "+
-        "from public.comment join public.font using(font_id) join public.user using(user_id) left join rating ON comment.comment_id = rating.comment_id  where public.user.username = $1 group by comment.comment_id;",
+const   getBase =  "select username,comment_text,comment.comment_id,public.user.user_id, AVG(rating.rating) as \"rating\", count(rating.rating) as \"total votes\" "+
+"from public.comment join public.font using(font_id) join public.user using(user_id) left join rating ON comment.comment_id = rating.comment_id ",
 
-    getFromFontIdQuery ="select username,comment_text,comment.comment_id,public.user.user_id, AVG(rating.rating) as \"rating\", count(rating.rating) as \"total votes\" "+
-        "from public.comment join public.font using(font_id) join public.user using(user_id) left join rating ON comment.comment_id = rating.comment_id where public.font.font_id = $1 group by comment.comment_id, public.user.user_id;",
+    getFromNameQuery = getBase+" where public.user.username = $1 group by comment.comment_id, public.user.user_id;",
+
+    getFromFontIdQuery =getBase+" where public.font.font_id = $1 group by comment.comment_id, public.user.user_id;",
     
-        insertQuery="insert into public.comment (user_id,font_id,comment_text) values ($1,$2,$3);";
+        insertQuery="insert into public.comment (user_id,font_id,comment_text) values ($1,$2,$3) RETURNING comment_id;",
+    getQuery = getBase + "where comment.comment_id = $1 group by comment.comment_id, public.user.user_id;";
 
 
 function getFromUserName(uName){
@@ -16,7 +18,9 @@ function getFromUserName(uName){
 function getFromFontId(fid){
     return conn.execute(getFromFontIdQuery,[fid]);
 }
-
+function getFromId(id){
+    return conn.execute(getQuery,[id]);
+}
 function insertComment(uid,fid,text){
     return conn.execute(insertQuery,[uid,fid,text]);
 }
@@ -24,5 +28,6 @@ function insertComment(uid,fid,text){
 module.exports = {
     getFromUserName:getFromUserName,
     getFromFontId:getFromFontId,
-    insertComment:insertComment
+    insertComment:insertComment,
+    get:getFromId
 }
